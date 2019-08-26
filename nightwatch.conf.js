9548cd4b267a23a86b83f93582f1550be5151af8
NageshJoy007 <Nagesh.joy007@gmail.com>
2019-08-26 16:41:39 +0530

    Nagesh - fixing driverpath

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -28,7 +28,7 @@
           },
           webdriver: {
             port: 9515,
-            server_path: "node_modules/.bin/chromedriver" || "node_modules/chromedriver",
+            server_path: "node_modules/.bin/chromedriver" || "node_modules/chromedriver/lib/chromedriver",
             cli_args: ["--verbose"]
           },
           desiredCapabilities : {
@@ -40,7 +40,7 @@
       chrome : {
         webdriver: {
           port: 9515,
-          server_path: "node_modules/.bin/chromedriver" || "node_modules/chromedriver",
+          server_path: "node_modules/.bin/chromedriver" || "node_modules/chromedriver/lib/chromedriver",
           cli_args: ["--verbose"]
         },
   
